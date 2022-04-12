1990c48b577430064073028c709596afe0ab4423
Arkyris <theartfulkludger@gmail.com>
2022-04-12 18:07:12 -0400

    More styling 2

---
+++ src/HowTo.svelte
@@ -0,0 +1,128 @@
+<div id="how_to_bg">
+    <div id="how_to_title">
+        <h1>How To</h1>
+    </div>
+    <div id="how_to">
+        <div id="actions">
+            <div class="action">
+                <div class="action_title">
+                    <h2>Rent</h2>
+                    <p>
+                        To rent cpu select the rent action. You can rent to
+                        yourself or another person. Leave the recipient field
+                        blank for self rental (optional). Use the slider to
+                        select rental time. From 12 hours to 6 days in 12 hour
+                        increments. Finally select the amount of wax you would
+                        like delegated to the recipient. The price will auto
+                        update on the submit button as you change the time and
+                        amount.
+                    </p>
+                </div>
+            </div>
+            <div class="action">
+                <div class="action_title">
+                    <h2>Add Rent</h2>
+                    <p>
+                        Use add rent when you want to add an amount to an
+                        already existing time frame.
+                    </p>
+                </div>
+            </div>
+            <div class="action">
+                <div class="action_title">
+                    <h2>Add Funds</h2>
+                    <p>
+                        When you rent an account is open on the contract in your
+                        name. You can add extra funds so they are ready to go
+                        the next time you need some cpu.
+                    </p>
+                </div>
+            </div>
+            <div class="action">
+                <div class="action_title">
+                    <h2>Withdraw Funds</h2>
+                    <p>
+                        You can withdraw some or all funds from your account at
+                        any time.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-family: "neoncity";
+        color: #fff;
+        font-size: 5vh;
+        font-weight: 1;
+        padding: 1.5vh;
+        margin: 0;
+        letter-spacing: 0.6vw;
+        text-shadow: 0 0 0.5vh #fff, 0 0 1vh #fff, 0 0 1vh #0ff, 0 0 1.5vh #0ff,
+            0 0 2vh #0ff, 0 0 3vh #0ff, 0 0 4vh #0ff, 0 0 5vh #0ff;
+    }
+    h2 {
+        font-family: "neoncity";
+        color: #fff;
+        font-size: 4vh;
+        font-weight: 1;
+        color: rgb(205, 251, 255);
+        text-shadow: 0 0 1vh #fff;
+        margin: 0;
+        letter-spacing: 0.6vw;
+        text-shadow: 0 0 0.5vh #fff, 0 0 1vh #fff, 0 0 1vh #f0f, 0 0 1.5vh #f0f,
+            0 0 2vh #f0f, 0 0 3vh #f0f, 0 0 4vh #f0f, 0 0 5vh #f0f;
+    }
+
+    p {
+        font-size: 1vw;
+        color: rgb(205, 251, 255);
+        text-shadow: 0 0 1vh #fff;
+        margin-top: 0;
+    }
+
+    #how_to_bg {
+        display: flex;
+        align-content: center;
+        flex-wrap: wrap;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    #how_to_title {
+        position: relative;
+        width: auto;
+        height: auto;
+        border-radius: 3vh;
+        box-shadow: 0 0 0.5vh #fff, 0 0 1vh #fff, 0 0 1vh #8080ff,
+            0 0 2vh #8080ff, 0 0 4vh #8080ff, inset 0 0 0.5vh #fff,
+            inset 0 0 1vh #fff, 0 0 1vh #8080ff, 0 0 2vh #8080ff;
+    }
+
+    #how_to {
+        position: relative;
+        display: flex;
+        width: auto;
+        height: auto;
+    }
+
+    #actions {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .action {
+        position: relative;
+        max-width: 97%;
+        width: auto;
+        height: auto;
+        margin-top: 2vh;
+        border-radius: 3vh;
+    }
+</style>
--- src/LoginButton.svelte
+++ src/LoginButton.svelte
@@ -61,7 +61,8 @@
         ual_button.style.animationName = "inherit";
         ual_button.style.animationDuration = "4s";
         ual_button.style.animationIterationCount = "infinite";
-        ual_button.style.textShadow = "0 0 1px #FFA600, 0 0 3px #FFA600, 0 0 5px #FFA600, 0 0 10px #FFA600";
+        ual_button.style.textShadow =
+            "0 0 1px #FFA600, 0 0 3px #FFA600, 0 0 5px #FFA600, 0 0 10px #FFA600";
         ual_button.style.fontFamily = "neoncity";
         ual_button.style.fontSize = "1.5vw";
         ual_button.style.borderStyle = "solid";
@@ -95,7 +96,7 @@
 <div class="button">
     <div id="login" />
     <button id="logout_button">Logout</button>
-    <div id={ual_button} class="ualbutton"></div>
+    <div id={ual_button} class="ualbutton" />
 </div>
 
 <style>
@@ -104,14 +105,20 @@
     }
 
     #logout_button {
-        background-color: #456663;
-        border-color: #000000;
-        color: aquamarine;
+        background-color: #000;
+        border-color: #fff;
+        color: white;
+        font-family: "neoncity";
+        font-size: 1.5vw;
+        letter-spacing: .1vw;
         text-align: center;
         border-radius: 2.5vh;
         width: 8vw;
         height: 5vh;
         display: none;
+        box-shadow: 0 0 1vh #fff, 0 0 2vh #fff, 0 0 2vh red, 0 0 4vh red,
+            0 0 5vh red, inset 0 0 1vh #fff, inset 0 0 2vh #fff,
+            inset 0 0 2vh red, inset 0 0 4vh red;
     }
     #login {
         border-radius: 2.5vh;
@@ -120,235 +127,151 @@
 
     @keyframes loginAnimation {
         from {
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.25rem 0.0rem 10px 2px #f0f,
-            inset -0.25rem 0.0rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.25rem 0.0rem 10px 2px #f0f,
-            0.25rem 0.0rem 10px 6px #0ff;
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.25rem 0rem 10px 2px #f0f,
+                inset -0.25rem 0rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.25rem 0rem 10px 2px #f0f,
+                0.25rem 0rem 10px 6px #0ff;
         }
-        5%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.2rem 0.05rem 10px 2px #f0f,
-            inset -0.2rem -0.05rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.2rem -0.05rem 10px 2px #f0f,
-            0.2rem 0.05rem 10px 6px #0ff;
+        5% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.2rem 0.05rem 10px 2px #f0f,
+                inset -0.2rem -0.05rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.2rem -0.05rem 10px 2px #f0f,
+                0.2rem 0.05rem 10px 6px #0ff;
         }
-        10%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.15rem 0.1rem 10px 2px #f0f,
-            inset -0.15rem -0.1rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.15rem -0.1rem 10px 2px #f0f,
-            0.15rem 0.1rem 10px 6px #0ff;
+        10% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.15rem 0.1rem 10px 2px #f0f,
+                inset -0.15rem -0.1rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.15rem -0.1rem 10px 2px #f0f,
+                0.15rem 0.1rem 10px 6px #0ff;
         }
-        15%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.1rem 0.15rem 10px 2px #f0f,
-            inset -0.1rem -0.15rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.1rem -0.15rem 10px 2px #f0f,
-            0.1rem 0.15rem 10px 6px #0ff;
+        15% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.1rem 0.15rem 10px 2px #f0f,
+                inset -0.1rem -0.15rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.1rem -0.15rem 10px 2px #f0f,
+                0.1rem 0.15rem 10px 6px #0ff;
         }
-        20%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.05rem 0.2rem 10px 2px #f0f,
-            inset -0.05rem -0.2rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.05rem -0.2rem 10px 2px #f0f,
-            0.05rem 0.2rem 10px 6px #0ff;
+        20% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.05rem 0.2rem 10px 2px #f0f,
+                inset -0.05rem -0.2rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.05rem -0.2rem 10px 2px #f0f,
+                0.05rem 0.2rem 10px 6px #0ff;
         }
-        25%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.0rem 0.25rem 10px 2px #f0f,
-            inset 0.0rem -0.25rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.0rem -0.25rem 10px 2px #f0f,
-            0.0rem 0.25rem 10px 6px #0ff;
+        25% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0rem 0.25rem 10px 2px #f0f,
+                inset 0rem -0.25rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0rem -0.25rem 10px 2px #f0f,
+                0rem 0.25rem 10px 6px #0ff;
         }
-        30%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.05rem 0.2rem 10px 2px #f0f,
-            inset 0.05rem -0.2rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.05rem -0.2rem 10px 2px #f0f,
-            -0.05rem 0.2rem 10px 6px #0ff;
+        30% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.05rem 0.2rem 10px 2px #f0f,
+                inset 0.05rem -0.2rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.05rem -0.2rem 10px 2px #f0f,
+                -0.05rem 0.2rem 10px 6px #0ff;
         }
-        35%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.1rem 0.15rem 10px 2px #f0f,
-            inset 0.1rem -0.15rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.1rem -0.15rem 10px 2px #f0f,
-            -0.1rem 0.15rem 10px 6px #0ff;
+        35% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.1rem 0.15rem 10px 2px #f0f,
+                inset 0.1rem -0.15rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.1rem -0.15rem 10px 2px #f0f,
+                -0.1rem 0.15rem 10px 6px #0ff;
         }
-        40%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.15rem 0.1rem 10px 2px #f0f,
-            inset 0.15rem -0.1rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.15rem -0.1rem 10px 2px #f0f,
-            -0.15rem 0.1rem 10px 6px #0ff;
+        40% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.15rem 0.1rem 10px 2px #f0f,
+                inset 0.15rem -0.1rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.15rem -0.1rem 10px 2px #f0f,
+                -0.15rem 0.1rem 10px 6px #0ff;
         }
-        45%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.2rem 0.05rem 10px 2px #f0f,
-            inset 0.2rem -0.05rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.2rem -0.05rem 10px 2px #f0f,
-            -0.2rem 0.05rem 10px 6px #0ff;
+        45% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.2rem 0.05rem 10px 2px #f0f,
+                inset 0.2rem -0.05rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.2rem -0.05rem 10px 2px #f0f,
+                -0.2rem 0.05rem 10px 6px #0ff;
         }
         50% {
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.25rem 0.0rem 10px 2px #f0f,
-            inset 0.25rem 0.0rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.25rem 0.0rem 10px 2px #f0f,
-            -0.25rem 0.0rem 10px 6px #0ff;
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.25rem 0rem 10px 2px #f0f,
+                inset 0.25rem 0rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.25rem 0rem 10px 2px #f0f,
+                -0.25rem 0rem 10px 6px #0ff;
         }
-        55%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-             inset -0.2rem -0.05rem 10px 2px #f0f,
-            inset 0.2rem 0.05rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.2rem 0.05rem 10px 2px #f0f,
-            -0.2rem -0.05rem 10px 6px #0ff;
+        55% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.2rem -0.05rem 10px 2px #f0f,
+                inset 0.2rem 0.05rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.2rem 0.05rem 10px 2px #f0f,
+                -0.2rem -0.05rem 10px 6px #0ff;
         }
-        60%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.15rem -0.1rem 10px 2px #f0f,
-            inset 0.15rem 0.1rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.15rem 0.1rem 10px 2px #f0f,
-            -0.15rem -0.1rem 10px 6px #0ff;
+        60% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.15rem -0.1rem 10px 2px #f0f,
+                inset 0.15rem 0.1rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.15rem 0.1rem 10px 2px #f0f,
+                -0.15rem -0.1rem 10px 6px #0ff;
         }
-        65%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.1rem -0.15rem 10px 2px #f0f,
-            inset 0.1rem 0.15rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.1rem 0.15rem 10px 2px #f0f,
-           -0.1rem -0.15rem 10px 6px #0ff;
+        65% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.1rem -0.15rem 10px 2px #f0f,
+                inset 0.1rem 0.15rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.1rem 0.15rem 10px 2px #f0f,
+                -0.1rem -0.15rem 10px 6px #0ff;
         }
-        70%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset -0.05rem -0.2rem 10px 2px #f0f,
-            inset 0.05rem 0.2rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.05rem 0.2rem 10px 2px #f0f,
-            -0.05rem -0.2rem 10px 6px #0ff;
+        70% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset -0.05rem -0.2rem 10px 2px #f0f,
+                inset 0.05rem 0.2rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0.05rem 0.2rem 10px 2px #f0f,
+                -0.05rem -0.2rem 10px 6px #0ff;
         }
-        75%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.0rem -0.25rem 10px 2px #f0f,
-            inset 0.0rem 0.25rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            0.0rem 0.25rem 10px 2px #f0f,
-            0.0rem -0.25rem 10px 6px #0ff;
+        75% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0rem -0.25rem 10px 2px #f0f,
+                inset 0rem 0.25rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ 0rem 0.25rem 10px 2px #f0f,
+                0rem -0.25rem 10px 6px #0ff;
         }
-        80%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.05rem -0.2rem 10px 2px #f0f,
-            inset -0.05rem 0.2rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.05rem 0.2rem 10px 2px #f0f,
-            0.05rem -0.2rem 10px 6px #0ff;
+        80% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.05rem -0.2rem 10px 2px #f0f,
+                inset -0.05rem 0.2rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.05rem 0.2rem 10px 2px #f0f,
+                0.05rem -0.2rem 10px 6px #0ff;
         }
-        85%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.1rem -0.15rem 10px 2px #f0f,
-            inset -0.1rem 0.15rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.1rem 0.15rem 10px 2px #f0f,
-            0.1rem -0.15rem 10px 6px #0ff;
+        85% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.1rem -0.15rem 10px 2px #f0f,
+                inset -0.1rem 0.15rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.1rem 0.15rem 10px 2px #f0f,
+                0.1rem -0.15rem 10px 6px #0ff;
         }
-        90%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.15rem -0.1rem 10px 2px #f0f,
-            inset -0.15rem 0.1rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.15rem 0.1rem 10px 2px #f0f,
-            0.15rem -0.1rem 10px 6px #0ff;
+        90% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.15rem -0.1rem 10px 2px #f0f,
+                inset -0.15rem 0.1rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.15rem 0.1rem 10px 2px #f0f,
+                0.15rem -0.1rem 10px 6px #0ff;
         }
-        95%{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.2rem -0.05rem 10px 2px #f0f,
-            inset -0.2rem 0.05rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.2rem 0.05rem 10px 2px #f0f,
-            0.2rem -0.05rem 10px 6px #0ff;
+        95% {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.2rem -0.05rem 10px 2px #f0f,
+                inset -0.2rem 0.05rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.2rem 0.05rem 10px 2px #f0f,
+                0.2rem -0.05rem 10px 6px #0ff;
         }
-        to{
-            box-shadow:
-            inset 0 0 6px #fff,
-            0 0 6px #fff,
-            /*Pink and Blue In*/
-            inset 0.25rem 0.0rem 10px 2px #f0f,
-            inset -0.25rem 0.0rem 10px 4px #0ff,
-            /*Pink and Blue Out*/
-            -0.25rem 0.0rem 10px 2px #f0f,
-            0.25rem 0.0rem 10px 6px #0ff;
+        to {
+            box-shadow: inset 0 0 6px #fff, 0 0 6px #fff,
+                /*Pink and Blue In*/ inset 0.25rem 0rem 10px 2px #f0f,
+                inset -0.25rem 0rem 10px 4px #0ff,
+                /*Pink and Blue Out*/ -0.25rem 0rem 10px 2px #f0f,
+                0.25rem 0rem 10px 6px #0ff;
         }
     }
 </style>
